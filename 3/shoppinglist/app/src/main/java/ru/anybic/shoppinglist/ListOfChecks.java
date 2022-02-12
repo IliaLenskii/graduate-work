78deb67438478432abbe665895b1812382ac5557
Ilia Lenskii <il.lenskii@gmail.com>
2022-02-12 22:52:30 +0300

    add app

---
+++ 3/shoppinglist/app/src/main/java/ru/anybic/shoppinglist/ListOfChecks.java
@@ -0,0 +1,14 @@
+package ru.anybic.shoppinglist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListOfChecks extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.list_of_checks);
+    }
+}
---
+++ 3/shoppinglist/app/src/main/java/ru/anybic/shoppinglist/TakePhotos.java
@@ -0,0 +1,14 @@
+package ru.anybic.shoppinglist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TakePhotos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.take_photos);
+    }
+}
