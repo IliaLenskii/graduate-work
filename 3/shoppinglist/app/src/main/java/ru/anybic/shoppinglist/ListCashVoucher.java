59012207e23f474e57561c41392aff7bdc00744b
Ilia Lenskii <il.lenskii@gmail.com>
2022-02-13 22:35:13 +0300

    Добавил новые классы и уточнил некоторые активити (экраны)

---
+++ 3/shoppinglist/app/src/main/java/ru/anybic/shoppinglist/ListCashVoucher.java
@@ -0,0 +1,14 @@
+package ru.anybic.shoppinglist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListCashVoucher extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.list_cash_voucher);
+    }
+}
