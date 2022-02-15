ba4fccc154039d01e34f0ba2910e221a6ea01725
Ilia Lenskii <il.lenskii@gmail.com>
2022-02-15 21:15:44 +0300

    Правки по тексту, новые диаграммы и чуть-чуть java

--- 3/shoppinglist/app/src/main/java/ru/anybic/shoppinglist/ListCashVoucher.java
+++ 3/shoppinglist/app/src/main/java/ru/anybic/shoppinglist/ListCashVoucher.java
@@ -3,12 +3,24 @@ package ru.anybic.shoppinglist;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class ListCashVoucher extends AppCompatActivity {
+
+    private String[] countries = {"На всю неделю", "ДР Алексея П.", "14 февраля"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.list_cash_voucher);
+
+
+        ListView countriesList = findViewById(R.id.voucherList);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter(this,
+                android.R.layout.simple_list_item_1, countries);
+
+        countriesList.setAdapter(adapter);
     }
 }
--- 3/shoppinglist/app/src/main/java/ru/anybic/shoppinglist/Main.java
+++ 3/shoppinglist/app/src/main/java/ru/anybic/shoppinglist/Main.java
@@ -2,7 +2,11 @@ package ru.anybic.shoppinglist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class Main extends AppCompatActivity {
 
@@ -10,5 +14,13 @@ public class Main extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+    }
+
+    public void openIntentListCashVoucher(View view) {
+
+        Intent intent1 = new Intent(this, ListCashVoucher.class);
+
+        startActivity( intent1 );
     }
 }
